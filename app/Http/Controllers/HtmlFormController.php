0289803852b27a481b476a1f868276f6dc732737
DaRkSoUl1690 <tiwarivedant.1690@gmail.com>
2022-06-08 19:49:54 +0530

    html form validation

--- app/Http/Controllers/HtmlFormController.php
+++ app/Http/Controllers/HtmlFormController.php
@@ -7,8 +7,14 @@ use Illuminate\Http\Request;
 class HtmlFormController extends Controller
 {
     //
-    function getData(Request $req){
-         
+    function getData(Request $req)
+    {
+
+        $req->validate([
+            'username' => 'required | max:10',
+            'password' => 'required | min:3',
+
+        ]);
         return $req->input();
         // echo "form submitted";
     }
--- resources/views/formUsers.blade.php
+++ resources/views/formUsers.blade.php
@@ -1,8 +1,13 @@
+
 <form action="users" method="POST">
     @csrf
    <input type="text" name = "username">
-   <br><br>
+   <br>
+   <span style="color:red;font-weight:bold">@error('username'){{$message}}@enderror</span>
+   <br>
    <input type="password" name = "password">
-   <br><br>
+   <br>
+   <span style="color:red;font-weight:bold">@error('password'){{$message}}@enderror</span>
+   <br>
     <button type="submit">login</button>
 </form>
